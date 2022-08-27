31b5b22558e2e81f1fa352cde10fc842a48286f8
leeanskey <1013061673@qq.com>
2022-08-27 20:37:17 +0800

    second commit

--- src/main/java/com/atguigu/springboot/SpringbootApplication.java
+++ src/main/java/com/atguigu/springboot/SpringbootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootApplication {
 
+    //asdsad
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
